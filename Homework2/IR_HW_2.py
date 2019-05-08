a9099911d5a9d54a508fb6e6c5332502aa45a74e
Ishan Gupta <ishangupta.work@gmail.com>
2019-05-07 21:25:30 -0700

    Clarification for teleport factor variable.
    Added comment to explain what does teleport_factor means, which is leap probability.

--- Homework2/IR_HW_2.py
+++ Homework2/IR_HW_2.py
@@ -62,7 +62,7 @@ def generate_original_rank_vector(graph):
 ##################################
 def generate_A_matrix(graph, m_matrix, damp_factor):
 
-    teleport_factor = (1 - damp_factor) / len(graph)
+    teleport_factor = (1 - damp_factor) / len(graph) # this is leap propbability
     a_matrix = damp_factor * m_matrix + teleport_factor
 
     return a_matrix
@@ -97,7 +97,7 @@ def power_converge_a_matrix(graph, rank_vector, m_matrix, damp_factor):
     rank_vector_t = rank_vector
     rank_vector_t_plus_1 = np.empty(shape=(len(rank_vector), 1))
     rank_vector_t_plus_1.fill(0)
-    teleport_factor = (1 - damp_factor) / len(graph)
+    teleport_factor = (1 - damp_factor) / len(graph) # this is leap propbability
 
     while not np.allclose(rank_vector_t, rank_vector_t_plus_1, rtol=1e-06, atol=1e-06):
         rank_vector_t = rank_vector
